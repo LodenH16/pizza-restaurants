72fc8ef7e177c1470e8af4e449a96b801c014ec4
Thomas Harris <loden16harris@gmail.com>
2022-01-24 21:33:20 -0600

    feat: implement search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import "./App.css";
 import { StudentGlobalStyle } from "./styles";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Student from "./components/Student";
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [students, setStudents] = useState([]);
+  const [filteredStudents, setFilteredStudents] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch("https://api.hatchways.io/assessment/students")
       .then((res) => res.json())
-      .then((data) => setData(data.students));
+      .then((data) => setStudents(data.students));
   }, []);
-  console.log(data);
+
+  const searchStudents = (searchValue) => {
+    //filter the students
+    const filteredList = students.filter((student) => {
+      return (
+        student.firstName.toUpperCase().includes(searchValue.toUpperCase()) ||
+        student.lastName.toUpperCase().includes(searchValue.toUpperCase())
+      );
+    });
+    console.log("SearchValue: ", searchValue);
+    switch (true) {
+      case /\W/.test(searchValue):
+        console.log("here");
+        setErrorMessage("Error: Invalid Character");
+        setFilteredStudents(students);
+        break;
+      case searchValue !== "" && filteredList.length > 0:
+        setErrorMessage(null);
+        setFilteredStudents(filteredList);
+        break;
+      case searchValue !== "" && filteredList.length === 0:
+        setErrorMessage("No results");
+        setFilteredStudents(filteredList);
+        break;
+      default:
+        setFilteredStudents(students);
+        setErrorMessage(null);
+        break;
+    }
+  };
+
   return (
     <div className="App">
+      <input
+        placeholder="Search by name"
+        onChange={(event) => searchStudents(event.target.value)}
+      ></input>
+      {errorMessage && <p>{errorMessage}</p>}
       <StudentGlobalStyle />
-      {data && data.map((data) => <Student data={data} />)}
+      {filteredStudents &&
+        filteredStudents.map((data) => <Student data={data} />)}
     </div>
   );
 };
